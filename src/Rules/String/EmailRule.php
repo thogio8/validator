7f944be016b4d2dd2841a04872a8b885a7376a9c
Thomas Gioana* <thomas.gioana@epitech.eu>
2025-03-30 16:16:17 +0200

    [FEAT] Create a rule for email

---
+++ src/Rules/String/EmailRule.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ValidatorPro\Rules\String;
+
+use ValidatorPro\Rules\AbstractRule;
+
+class EmailRule extends AbstractRule
+{
+    /**
+     * @var string
+     */
+    protected string $message = 'Le champ :attribute doit être une adresse email valide';
+
+    /**
+     * Validate if the value is a valid email address
+     *
+     * @param mixed $value
+     * @param array<string, mixed> $parameters
+     * @param array<string, mixed> $data
+     * @return bool
+     */
+    public function validate($value, array $parameters = [], array $data = []): bool
+    {
+        // Vérifie que la valeur est une chaîne
+        if (! is_string($value)) {
+            return false;
+        }
+
+        // Utilise le filtre de validation d'email de PHP
+        return filter_var($value, FILTER_VALIDATE_EMAIL) !== false;
+    }
+}
